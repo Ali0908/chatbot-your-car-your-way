247de8fa88669dec30c1856a89cadb6ca032b3b9
Ali0908 <zaabat97@gmail.com>
2025-03-14 08:40:01 +0100

    feat: v1.5 wip

--- back/src/main/resources/response.sql
+++ back/src/main/resources/response.sql
@@ -1,11 +1,11 @@
-INSERT INTO response (id, question, answer) VALUES
-                                               (1, 'comment ça va', 'je vais bien, merci, comment ça va'),
-                                               (2, 'quel est votre nom', 'je suis un chatbot'),
-                                               (3, 'quelle heure est-il', 'je ne peux pas donner l''heure exacte, désolé'),
-                                               (4, 'où êtes-vous situé', 'je suis un programme informatique, je n''ai pas de localisation physique'),
-                                               (5, 'quel est le numéro de service clientèle', 'le numéro de service clientèle est 06 06 06 06 06'),
-                                               (6, 'quels sont vos horaires d''ouverture', 'nous sommes ouverts de 9h à 18h du lundi au vendredi'),
-                                               (7, 'comment puis-je vous aider', 'je suis ici pour répondre à vos questions'),
-                                               (8, 'pouvez-vous m''aider', 'bien sûr, je suis là pour ça'),
-                                               (9, 'pouvez-vous me donner plus d''informations', 'bien sûr, que voulez-vous savoir'),
-                                               (10, 'comment puis-je vous contacter', 'vous pouvez nous contacter par téléphone ou par email');
+INSERT INTO response (question, answer) VALUES
+                                            ('comment ça va', 'je vais bien, merci, comment ça va'),
+                                            ('quel est votre nom', 'je suis un chatbot'),
+                                            ('quelle heure est-il', 'je ne peux pas donner l''heure exacte, désolé'),
+                                            ('où êtes-vous situé', 'je suis un programme informatique, je n''ai pas de localisation physique'),
+                                            ('quel est le numéro de service clientèle', 'le numéro de service clientèle est 06 06 06 06 06'),
+                                            ('quels sont vos horaires d''ouverture', 'nous sommes ouverts de 9h à 18h du lundi au vendredi'),
+                                            ('comment puis-je vous aider', 'je suis ici pour répondre à vos questions'),
+                                            ('pouvez-vous m''aider', 'bien sûr, je suis là pour ça'),
+                                            ('pouvez-vous me donner plus d''informations', 'bien sûr, que voulez-vous savoir'),
+                                            ('comment puis-je vous contacter', 'vous pouvez nous contacter par téléphone ou par email');
